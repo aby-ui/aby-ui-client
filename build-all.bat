78cb6397451b3c06902b5923c869a7bf0507ee6a
aby-ui <aby_ui@163.com>
2018-07-26 03:26:53 +0800

    mac

--- build-all.bat
+++ build-all.bat
@@ -1,6 +1,8 @@
 set ELECTRON_MIRROR=https://npm.taobao.org/mirrors/electron/
 call electron-packager .\app AbyUI --executable-name=爱不易插件 --platform=win32 --arch=x64 --app-version 1.0.0 --icon 163UI-light.ico --asar --out=_package --overwrite
 
+call electron-packager .\app AbyUI --executable-name=爱不易插件 --platform=darwin --arch=x64 --app-version 1.0.0 --icon logo.icns --asar --out=_package --overwrite
+
 call build-app.bat
 call build-lib.bat
 call build-electron-hack.bat
